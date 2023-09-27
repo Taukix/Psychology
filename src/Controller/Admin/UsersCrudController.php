c6a8e3c9f9996353ed6fcc30f3e7556160833b2b
tommy <tommyboisseau03@gmail.com>
2023-09-27 17:14:19 +0200

    Feat (Users & EasyAdmin) Add

---
+++ src/Controller/Admin/UsersCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+
+class UsersCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Users::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('email'),
+            TextField::new('password'),
+            ArrayField::new('roles'),
+        ];
+    }
+}
